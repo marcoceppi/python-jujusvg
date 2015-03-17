ebd9089bf787187c02bc78e17d88ed4e9d37481d
Marco Ceppi <marco@ceppi.net>
2015-03-17 03:54:02 -0400

    first pass

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Not much to say... yet
+This was an attempt to use pygo to get a Python Module version of the jujusvg binary. That failed miserably so instead it's just the example go file from jujusvg with an installable package.
+
+```
+make deps
+go get -v github.com/marcoceppi/python-jujusvg
+$GOPATH/bin/python-jujusvg <bundle-file>
+```
---
+++ pyjujusvg.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "io/ioutil"
+    "log"
+    "os"
+    "strings"
+
+    "gopkg.in/errgo.v1"
+    "gopkg.in/juju/charm.v5-unstable"
+
+    // Import the jujusvg library and the juju charm library
+    "github.com/juju/jujusvg"
+)
+
+// iconURL takes a reference to a charm and returns the URL for that charm's icon.
+// In this case, we're using the api.jujucharms.com API to provide the icon's URL.
+func iconURL(ref *charm.Reference) string {
+    return "https://api.jujucharms.com/v4/" + ref.Path() + "/archive/icon.svg"
+}
+
+func generate(bundle_file string) (*jujusvg.Canvas, error) {
+    // First, we need to read our bundle data into a []byte
+    bundle_data, err := ioutil.ReadFile(bundle_file)
+    if err != nil {
+        return nil, errgo.Newf("Error reading bundle: %s", err)
+    }
+
+    // Next, generate a charm.Bundle from the bytearray by passing it to ReadNewBundleData.
+    // This gives us an in-memory object representation of the bundle that we can pass to jujusvg
+    bundle, err := charm.ReadBundleData(strings.NewReader(string(bundle_data)))
+    if err != nil {
+        return nil, errgo.Newf("Error parsing bundle: %s\n", err)
+    }
+
+    // Next, build a canvas of the bundle.  This is a simplified version of a charm.Bundle
+    // that contains just the position information and charm icon URLs necessary to build
+    // the SVG representation of the bundle
+    canvas, err := jujusvg.NewFromBundle(bundle, iconURL)
+    if err != nil {
+        return nil, errgo.Newf("Error generating canvas: %s\n", err)
+    }
+
+    return canvas, nil
+}
+
+func main() {
+    if len(os.Args) != 2 {
+        log.Fatalf("Please provide the path of a bundle as the first argument")
+    }
+    canvas, err := generate(os.Args[1])
+
+    if err != nil {
+        log.Fatalf("FAIL: %s\n", err)
+    }
+
+    // Finally, marshal that canvas as SVG to os.Stdout; this will print the
+    // SVG data required to generate an image of the bundle.
+    canvas.Marshal(os.Stdout)
+}
